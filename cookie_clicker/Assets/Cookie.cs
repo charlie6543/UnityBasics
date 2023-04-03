8ed3a4a3c3cbc8261121cd17ef26e7f88b85bc6a
charlie6543 <114709092+charlie6543@users.noreply.github.com>
2023-04-02 20:06:49 -0700

    Assets folder of cookie_clicker

---
+++ cookie_clicker/Assets/Cookie.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Cookie : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+    private int cookieCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void ClickCookie()
+    {
+        string msg = "";
+        cookieCount++;
+        msg += cookieCount;
+        text.text = msg;
+    }
+
+    public int getCount(){
+        return cookieCount;
+    }
+
+    public bool subtractCount(int toSubtract){
+        if(toSubtract < 0) return false;
+        if(cookieCount >= toSubtract){
+            cookieCount -= toSubtract;
+            string msg = "" + cookieCount;
+            text.text = msg;
+            return true;
+        }
+        else return false;
+    }
+}
