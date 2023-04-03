7bbf6c2c6e5502012f3b1635c14ad573122b2d25
charlie6543 <114709092+charlie6543@users.noreply.github.com>
2023-04-02 19:52:34 -0700

    Adding write_out_text files

---
+++ write_out_text/Assets/ActivateWriter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ActivateWriter : MonoBehaviour
+{
+    [SerializeField] private TextWriter textWriter;
+    private Text messageText;
+
+    private void Awake()
+    {
+        messageText = transform.Find("Message").GetComponent<Text>();
+    }
+
+    void Start()
+    {
+        textWriter.AddWriter(messageText, "Once upon a time, in a galaxy far, far, away, there lived a lonely duck. " +
+            "The duck had lived sheltered in a castle all of its life.", 0.03f, true);
+    }
+
+    
+}
